d4650b951f2c193089ffd17f315a1a1173e82207
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2019-11-21 15:14:38 +0700

    update desain

--- dump.sql
+++ dump.sql
@@ -16,11 +16,11 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`sipak` /*!40100 DEFAULT CHARACTER SET l
 
 USE `sipak`;
 
-/*Table structure for table `bataskategori` */
+/*Table structure for table `batas_kategori` */
 
-DROP TABLE IF EXISTS `bataskategori`;
+DROP TABLE IF EXISTS `batas_kategori`;
 
-CREATE TABLE `bataskategori` (
+CREATE TABLE `batas_kategori` (
   `ID_JABATAN` int(11) NOT NULL,
   `ID_KATEGORI` int(11) NOT NULL,
   `MINIMAL` int(11) NOT NULL,
@@ -28,16 +28,16 @@ CREATE TABLE `bataskategori` (
   PRIMARY KEY (`ID_JABATAN`,`ID_KATEGORI`),
   KEY `FK_BATASKAT_MEMILIKI_KATEGORI` (`ID_KATEGORI`),
   CONSTRAINT `FK_BATASKAT_BATAS_UNT_JABATAN` FOREIGN KEY (`ID_JABATAN`) REFERENCES `jabatan` (`ID_JABATAN`),
-  CONSTRAINT `FK_BATASKAT_MEMILIKI_KATEGORI` FOREIGN KEY (`ID_KATEGORI`) REFERENCES `kategoripenilaian` (`ID_KATEGORI`)
+  CONSTRAINT `FK_BATASKAT_MEMILIKI_KATEGORI` FOREIGN KEY (`ID_KATEGORI`) REFERENCES `kategori_penilaian` (`ID_KATEGORI`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `bataskategori` */
+/*Data for the table `batas_kategori` */
 
-/*Table structure for table `itempenilaian` */
+/*Table structure for table `item_penilaian` */
 
-DROP TABLE IF EXISTS `itempenilaian`;
+DROP TABLE IF EXISTS `item_penilaian`;
 
-CREATE TABLE `itempenilaian` (
+CREATE TABLE `item_penilaian` (
   `ID_ITEM` int(11) NOT NULL AUTO_INCREMENT,
   `ID_PAK` int(11) NOT NULL,
   `ID_UNSUR` int(11) NOT NULL,
@@ -51,10 +51,10 @@ CREATE TABLE `itempenilaian` (
   KEY `FK_ITEMPENI_MEMILIKI1_PAK` (`ID_PAK`),
   KEY `FK_ITEMPENI_MEMILIKI__UNSURPEN` (`ID_UNSUR`),
   CONSTRAINT `FK_ITEMPENI_MEMILIKI1_PAK` FOREIGN KEY (`ID_PAK`) REFERENCES `pak` (`ID_PAK`),
-  CONSTRAINT `FK_ITEMPENI_MEMILIKI__UNSURPEN` FOREIGN KEY (`ID_UNSUR`) REFERENCES `unsurpenilaian` (`ID_UNSUR`)
+  CONSTRAINT `FK_ITEMPENI_MEMILIKI__UNSURPEN` FOREIGN KEY (`ID_UNSUR`) REFERENCES `unsur_penilaian` (`ID_UNSUR`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `itempenilaian` */
+/*Data for the table `item_penilaian` */
 
 /*Table structure for table `jabatan` */
 
@@ -64,52 +64,54 @@ CREATE TABLE `jabatan` (
   `ID_JABATAN` int(11) NOT NULL AUTO_INCREMENT,
   `KREDIT` int(11) NOT NULL,
   `JABATAN` varchar(15) NOT NULL,
-  `GOLONGAN` varchar(5) NOT NULL,
   PRIMARY KEY (`ID_JABATAN`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 /*Data for the table `jabatan` */
 
-insert  into `jabatan`(`ID_JABATAN`,`KREDIT`,`JABATAN`,`GOLONGAN`) values 
-(1,150,'Asisten Ahli','III/b'),
-(2,200,'Lektor','III/c'),
-(3,300,'Lektor','III/d'),
-(4,400,'Lektor Kepala','IV/a'),
-(5,550,'Lektor Kepala','IV/b'),
-(6,700,'Lektor Kepala','IV/c'),
-(7,850,'Profesor','IV/d'),
-(8,1050,'Profesor','IV/e');
+insert  into `jabatan`(`ID_JABATAN`,`KREDIT`,`JABATAN`) values 
+(1,150,'Asisten Ahli II'),
+(2,200,'Lektor III/c'),
+(3,300,'Lektor III/d'),
+(4,400,'Lektor Kepala I'),
+(5,550,'Lektor Kepala I'),
+(6,700,'Lektor Kepala I'),
+(7,850,'Profesor IV/d'),
+(8,1050,'Profesor');
 
-/*Table structure for table `kategoripenilaian` */
+/*Table structure for table `kategori_penilaian` */
 
-DROP TABLE IF EXISTS `kategoripenilaian`;
+DROP TABLE IF EXISTS `kategori_penilaian`;
 
-CREATE TABLE `kategoripenilaian` (
+CREATE TABLE `kategori_penilaian` (
   `ID_KATEGORI` int(11) NOT NULL AUTO_INCREMENT,
   `NAMA` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`ID_KATEGORI`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `kategoripenilaian` */
+/*Data for the table `kategori_penilaian` */
 
-/*Table structure for table `logininfo` */
+/*Table structure for table `login_info` */
 
-DROP TABLE IF EXISTS `logininfo`;
+DROP TABLE IF EXISTS `login_info`;
 
-CREATE TABLE `logininfo` (
+CREATE TABLE `login_info` (
   `ID_USER` int(11) NOT NULL,
   `USERNAME` varchar(25) NOT NULL,
   `PASSWORD` varchar(25) NOT NULL,
   PRIMARY KEY (`ID_USER`),
+  UNIQUE KEY `UNIQUE_USERNAME` (`USERNAME`),
   CONSTRAINT `FK_LOGININF_RELATIONS_USER` FOREIGN KEY (`ID_USER`) REFERENCES `user` (`ID_USER`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `logininfo` */
+/*Data for the table `login_info` */
 
-insert  into `logininfo`(`ID_USER`,`USERNAME`,`PASSWORD`) values 
+insert  into `login_info`(`ID_USER`,`USERNAME`,`PASSWORD`) values 
 (1,'admin','admin'),
 (2,'penilai','penilai'),
-(3,'dosen','dosen');
+(3,'dosen','dosen'),
+(11,'asdfasdfasdf','asdfasdf'),
+(22,'123456','asdfasdf');
 
 /*Table structure for table `pak` */
 
@@ -141,42 +143,46 @@ CREATE TABLE `pak` (
 
 /*Data for the table `pak` */
 
-/*Table structure for table `penilailuar` */
+/*Table structure for table `penilai_luar` */
 
-DROP TABLE IF EXISTS `penilailuar`;
+DROP TABLE IF EXISTS `penilai_luar`;
 
-CREATE TABLE `penilailuar` (
+CREATE TABLE `penilai_luar` (
   `ID_USER` int(11) NOT NULL,
   `ID_JABATAN` int(11) NOT NULL,
-  `ID_SUB_RUMPUN` int(11) NOT NULL,
+  `ID_SUBRUMPUN` int(11) NOT NULL,
   `NIP` varchar(50) NOT NULL,
   `EMAIL` varchar(50) NOT NULL,
   `TELEPON` varchar(15) NOT NULL,
   `ASAL_INSTANSI` varchar(100) NOT NULL,
   PRIMARY KEY (`ID_USER`),
   KEY `FK_PENILAIL_MEMILIKI__JABATAN` (`ID_JABATAN`),
-  KEY `FK_PENILAIL_MEMILIKI__SUBRUMPU` (`ID_SUB_RUMPUN`),
+  KEY `FK_PENILAIL_MEMILIKI__SUBRUMPU` (`ID_SUBRUMPUN`),
   CONSTRAINT `FK_PENILAIL_ADALAH_USER` FOREIGN KEY (`ID_USER`) REFERENCES `user` (`ID_USER`),
   CONSTRAINT `FK_PENILAIL_MEMILIKI__JABATAN` FOREIGN KEY (`ID_JABATAN`) REFERENCES `jabatan` (`ID_JABATAN`),
-  CONSTRAINT `FK_PENILAIL_MEMILIKI__SUBRUMPU` FOREIGN KEY (`ID_SUB_RUMPUN`) REFERENCES `subrumpunilmu` (`ID_SUB_RUMPUN`)
+  CONSTRAINT `FK_PENILAIL_MEMILIKI__SUBRUMPU` FOREIGN KEY (`ID_SUBRUMPUN`) REFERENCES `subrumpun` (`ID_SUBRUMPUN`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `penilailuar` */
+/*Data for the table `penilai_luar` */
 
-/*Table structure for table `subrumpunilmu` */
+insert  into `penilai_luar`(`ID_USER`,`ID_JABATAN`,`ID_SUBRUMPUN`,`NIP`,`EMAIL`,`TELEPON`,`ASAL_INSTANSI`) values 
+(11,1,1,'asdfasdfasdf','asdfasdfasdf@gmail.com','asdfasdf','aasdfasdfasdf'),
+(22,4,4,'123456','yoga@uinsby.ac.id','yoyo','aasdfasdfasdf');
 
-DROP TABLE IF EXISTS `subrumpunilmu`;
+/*Table structure for table `subrumpun` */
 
-CREATE TABLE `subrumpunilmu` (
-  `ID_SUB_RUMPUN` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `subrumpun`;
+
+CREATE TABLE `subrumpun` (
+  `ID_SUBRUMPUN` int(11) NOT NULL AUTO_INCREMENT,
   `RUMPUN` varchar(25) NOT NULL,
-  `SUB_RUMPUN` varchar(25) NOT NULL,
-  PRIMARY KEY (`ID_SUB_RUMPUN`)
+  `SUBRUMPUN` varchar(25) NOT NULL,
+  PRIMARY KEY (`ID_SUBRUMPUN`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
-/*Data for the table `subrumpunilmu` */
+/*Data for the table `subrumpun` */
 
-insert  into `subrumpunilmu`(`ID_SUB_RUMPUN`,`RUMPUN`,`SUB_RUMPUN`) values 
+insert  into `subrumpun`(`ID_SUBRUMPUN`,`RUMPUN`,`SUBRUMPUN`) values 
 (1,'MIPA','IPA'),
 (2,'MIPA','Matematika'),
 (4,'Teknik','Ilmu Keteknikan Industri'),
@@ -185,11 +191,11 @@ insert  into `subrumpunilmu`(`ID_SUB_RUMPUN`,`RUMPUN`,`SUB_RUMPUN`) values
 (7,'Ekonomi','Ilmu Manajemen'),
 (8,'Seni, Desain, dan Media','Media');
 
-/*Table structure for table `unsurpenilaian` */
+/*Table structure for table `unsur_penilaian` */
 
-DROP TABLE IF EXISTS `unsurpenilaian`;
+DROP TABLE IF EXISTS `unsur_penilaian`;
 
-CREATE TABLE `unsurpenilaian` (
+CREATE TABLE `unsur_penilaian` (
   `ID_UNSUR` int(11) NOT NULL AUTO_INCREMENT,
   `ID_KATEGORI` int(11) NOT NULL,
   `NAMA` varchar(50) DEFAULT NULL,
@@ -199,10 +205,10 @@ CREATE TABLE `unsurpenilaian` (
   `BUKTI` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`ID_UNSUR`),
   KEY `FK_UNSURPEN_MEMILIKI__KATEGORI` (`ID_KATEGORI`),
-  CONSTRAINT `FK_UNSURPEN_MEMILIKI__KATEGORI` FOREIGN KEY (`ID_KATEGORI`) REFERENCES `kategoripenilaian` (`ID_KATEGORI`)
+  CONSTRAINT `FK_UNSURPEN_MEMILIKI__KATEGORI` FOREIGN KEY (`ID_KATEGORI`) REFERENCES `kategori_penilaian` (`ID_KATEGORI`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `unsurpenilaian` */
+/*Data for the table `unsur_penilaian` */
 
 /*Table structure for table `user` */
 
@@ -218,14 +224,16 @@ CREATE TABLE `user` (
   `KETERANGAN` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`ID_USER`),
   UNIQUE KEY `AK_IDENTIFIER_2` (`ID_PEGAWAI`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 /*Data for the table `user` */
 
 insert  into `user`(`ID_USER`,`ROLE`,`STATUS_USER`,`NAMA`,`ID_PEGAWAI`,`ANGKA_KREDIT`,`KETERANGAN`) values 
 (1,4,1,'Admin',NULL,NULL,NULL),
 (2,1,1,'Penilai Luar',NULL,NULL,NULL),
-(3,3,1,'Dosen',NULL,250,NULL);
+(3,3,1,'Dosen',NULL,250,NULL),
+(11,1,1,'asdfasdfasdf',NULL,NULL,NULL),
+(22,1,1,'yogayoga',NULL,NULL,NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
