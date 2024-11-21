d8d0d4c2e4c45de5104d893ec44d7e4388ec0e19
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2024-11-21 20:35:46 +0700

    synchronous ajax

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,11 +1,18 @@
 
+$.ajaxSetup({
+    headers: {
+        'X-Requested-With': 'XMLHttpRequest'
+    },
+    async: false,
+});
+
 var getUrl = window.location;
 var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1] + "/";
 
 function scrollTo(el){
-	$('html, body').animate({
-		scrollTop: $(el).offset().top
-	}, 750);
+    $('html, body').animate({
+        scrollTop: $(el).offset().top
+    }, 750);
 }
 
 function inputHasilSidang(idPAK){
@@ -28,24 +35,24 @@ function onInputHasilSidang(idPAK, setuju){
 
 var onConfirm = null;
 function askConfirmation(title, content, _onConfirm){
-	$modal = $("#modal-confirm");
-	$modal.find(".modal-title").text(title);
-	$modal.find(".modal-body").html(content);
-	onConfirm = _onConfirm;
-	$modal.modal("show");
+    $modal = $("#modal-confirm");
+    $modal.find(".modal-title").text(title);
+    $modal.find(".modal-body").html(content);
+    onConfirm = _onConfirm;
+    $modal.modal("show");
 }
 
 function showError(title, content){
-	$modal = $("#modal-error");
-	$modal.find(".modal-title").text(title);
-	$modal.find(".modal-body").html(content);
-	$modal.modal("show");
+    $modal = $("#modal-error");
+    $modal.find(".modal-title").text(title);
+    $modal.find(".modal-body").html(content);
+    $modal.modal("show");
 }
 function showNotif(title, content){
-	$modal = $("#modal-notif");
-	$modal.find(".modal-title").text(title);
-	$modal.find(".modal-body").html(content);
-	$modal.modal("show");
+    $modal = $("#modal-notif");
+    $modal.find(".modal-title").text(title);
+    $modal.find(".modal-body").html(content);
+    $modal.modal("show");
 }
 function validURL(str) {
   var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
@@ -58,68 +65,68 @@ function validURL(str) {
 }
 var onConfirmUrl = null;
 function askUrl(title, text, initialValue, onConfirm){
-	$modal = $("#modal-url");
-	$modal.find(".modal-title").html(title);
-	$modal.find(".modal-body p").html(text);
-	if(initialValue.startsWith("http")) $modal.find("#field-url").val(initialValue);
-	$btnConfirm = $modal.find(".btn-confirm");
-	onConfirmUrl = onConfirm;
-	$modal.modal("show");
+    $modal = $("#modal-url");
+    $modal.find(".modal-title").html(title);
+    $modal.find(".modal-body p").html(text);
+    if(initialValue.startsWith("http")) $modal.find("#field-url").val(initialValue);
+    $btnConfirm = $modal.find(".btn-confirm");
+    onConfirmUrl = onConfirm;
+    $modal.modal("show");
 }
 
 var onInput = null;
 function askInput(title, text, placeholder, initialValue, onConfirm){
-	$modal = $("#modal-input");
-	$modal.find(".modal-title").html(title);
-	$modal.find(".modal-body p").html(text);
-	$input = $modal.find(".modal-body input");
-	$input.attr("placeholder", placeholder);
-	$input.val(initialValue);
-	onInput = onConfirm;
-	$btnConfirm = $modal.find(".btn-confirm");
-	$modal.modal("show");
+    $modal = $("#modal-input");
+    $modal.find(".modal-title").html(title);
+    $modal.find(".modal-body p").html(text);
+    $input = $modal.find(".modal-body input");
+    $input.attr("placeholder", placeholder);
+    $input.val(initialValue);
+    onInput = onConfirm;
+    $btnConfirm = $modal.find(".btn-confirm");
+    $modal.modal("show");
 }
 
 $(document).ready(function(){
-	$modalInput = $("#modal-input")
-	$modalInput.find("#btn-confirm-input").click(function(){
-		let inpnut = $modalInput.find("input").val();
-		if(inpnut == ""){
-			showError("Error", "Input tidak boleh kosong");
-			return;
-		}
-		onInput(inpnut);
-	});
-	$(".dropdown-item").click(function(e){
-		if($(this).attr("href")=="#"){
-			e.preventDefault();
-		}
-	});
-	$(".dropdown-select .dropdown-item").click(function(){
-		$(this).parent().parent().find(".dropdown-item.selected").removeClass("selected");
-		$(this).addClass("selected");
-		$(this).parent().parent().find(".dropdown-toggle").text($(this).text());
-	});
-	$("#btn-confirm-url").click(function(){
-		$modal = $("#modal-url");
-		$field = $modal.find("#field-url");
-		let url = $field.val();
-		if(!validURL(url)){
-			alert("URL tidak valid");
-			return;
-		}
-		$modal.modal("hide");
-		$field.val("");
-		onConfirmUrl(url);
-	});
-	$("#btn-confirm").click(function(){
-		onConfirm();
-		$("#modal-confirm").modal("hide");
-	});
-	if (typeof error === 'string') {
-		showError("Error", error);
-	}else if (typeof error ===  'object'){
-		showError(error.title, error.message);
-	}
-	$(".table-datatable").DataTable();
+    $modalInput = $("#modal-input")
+    $modalInput.find("#btn-confirm-input").click(function(){
+        let inpnut = $modalInput.find("input").val();
+        if(inpnut == ""){
+            showError("Error", "Input tidak boleh kosong");
+            return;
+        }
+        onInput(inpnut);
+    });
+    $(".dropdown-item").click(function(e){
+        if($(this).attr("href")=="#"){
+            e.preventDefault();
+        }
+    });
+    $(".dropdown-select .dropdown-item").click(function(){
+        $(this).parent().parent().find(".dropdown-item.selected").removeClass("selected");
+        $(this).addClass("selected");
+        $(this).parent().parent().find(".dropdown-toggle").text($(this).text());
+    });
+    $("#btn-confirm-url").click(function(){
+        $modal = $("#modal-url");
+        $field = $modal.find("#field-url");
+        let url = $field.val();
+        if(!validURL(url)){
+            alert("URL tidak valid");
+            return;
+        }
+        $modal.modal("hide");
+        $field.val("");
+        onConfirmUrl(url);
+    });
+    $("#btn-confirm").click(function(){
+        onConfirm();
+        $("#modal-confirm").modal("hide");
+    });
+    if (typeof error === 'string') {
+        showError("Error", error);
+    }else if (typeof error ===  'object'){
+        showError(error.title, error.message);
+    }
+    $(".table-datatable").DataTable();
 })
